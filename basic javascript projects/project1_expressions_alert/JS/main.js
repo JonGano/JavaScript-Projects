0e75a8c3261232f0d72f4be688a31e7ced9bb8cd
JonGano <97046218+JonGano@users.noreply.github.com>
2022-01-26 11:04:59 -0500

    basic JS exercises

---
+++ basic javascript projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+alert("Help! I'm stuck inside this computer!")  //alert button informing user that the functions of the computer are 
+                                        //being carried out by a person stuck inside instead of the usual 1's and 0's of binary code.
+
+var carLot= "Dan's used autos", owner="devito", cars="10", trucks="4", vans="5" //created multiple variables in same line
+
+document.write("Welcome to " + carLot + ". I'm the owner Danny " + owner + ". We have plenty to choose from here including "
++ cars + " cars, " + trucks + " trucks, and " + vans + " vans.");//concatenated variables with strings to form a coherent meesage
+                                                                 //that can be altered depending on the change in 
+                                                                 //lot name, owner and inventory on the lot.
+
+document.write(" And if you're looking for a motorcycle, You're in luck! We have ")
+document.write(3+3);  //simple expression answer is shown at the end of the sentence above.
+
